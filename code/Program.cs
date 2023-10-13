0ce02e178c7c1f6b3c25290a334d9df930c616e1
Matt <matt@ninetyfour.dev>
2023-10-13 22:26:10 +0100

    Load devmap.scene by default

--- code/Program.cs
+++ code/Program.cs
@@ -1,18 +1,15 @@
-﻿using Sandbox;
-using System.Linq;
-
-public static class Program
+﻿public static class Program
 {
-	public static void Main()
-	{
-		//
-		// In the future this won't exist, we'll have an option for "default scene"
-		// and when you press play it'll load that.
-		//
+    public static void Main()
+    {
+        //
+        // In the future this won't exist, we'll have an option for "default scene"
+        // and when you press play it'll load that.
+        //
 
-		GameManager.ActiveScene = new Scene();
-		GameManager.ActiveScene.LoadFromFile( "turret.scene" );
+        GameManager.ActiveScene = new Scene();
+        GameManager.ActiveScene.LoadFromFile("devmap.scene");
 
-		GameManager.IsPlaying = true;
-	}
+        GameManager.IsPlaying = true;
+    }
 }
