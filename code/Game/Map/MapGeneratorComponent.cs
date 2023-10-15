c2f20d861af0fb3a5a06fff6c5bdcc7c1cca5021
Brian Lindenhof <brian@shadowbrain.net>
2023-10-15 10:38:24 +0200

    Map generator init

--- code/Game/Map/MapGeneratorComponent.cs
+++ code/Game/Map/MapGeneratorComponent.cs
@@ -1,12 +1,20 @@
 using Sandbox;
+using System.Collections.Generic;
 
 [Title( "Map Generator" )]
 [Category( "World" )]
 [Icon( "map", "red", "white" )]
 public sealed class MapGeneratorComponent : BaseComponent
 {
+	List<GameObject> gameObjects = new List<GameObject>() { };
+
+	public override void OnStart()
+	{
+		base.OnStart();
+	}
+
 	public override void Update()
 	{
-		
+
 	}
 }
