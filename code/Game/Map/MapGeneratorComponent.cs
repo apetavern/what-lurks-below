8ebb5d0bcbbc2c2bd3ba0d65213102f2b97c710a
Brian Lindenhof <brian@shadowbrain.net>
2023-10-15 10:32:48 +0200

    Initial map generator component and reconfigure the devmap

---
+++ code/Game/Map/MapGeneratorComponent.cs
@@ -0,0 +1,12 @@
+using Sandbox;
+
+[Title( "Map Generator" )]
+[Category( "World" )]
+[Icon( "map", "red", "white" )]
+public sealed class MapGeneratorComponent : BaseComponent
+{
+	public override void Update()
+	{
+		
+	}
+}
