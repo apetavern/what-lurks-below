782e26e97c4d4efe9001b206cf1d24b60cab549a
Brian Lindenhof <brian@shadowbrain.net>
2023-10-15 14:13:32 +0200

    Initial connections system and tweaks to the room spawning

--- code/Game/Map/MapGeneratorComponent.cs
+++ code/Game/Map/MapGeneratorComponent.cs
@@ -11,7 +11,7 @@ public sealed class MapGeneratorComponent : BaseComponent
 {
 	List<string> Rooms = new List<string>() { "prefabs/rooms/room_01.object" };
 
-	List<GameObject> SpawnedRooms = new List<GameObject>();
+	List<RoomChunkComponent> SpawnedRooms = new List<RoomChunkComponent>();
 
 	[Property] int RoomCount { get; set; } = 10;
 
@@ -20,7 +20,7 @@ public sealed class MapGeneratorComponent : BaseComponent
 	public override void OnStart()
 	{
 		var room1 = ResourceLibrary.Get<PrefabFile>( Rooms[0] );
-		SpawnedRooms.Add( SceneUtility.Instantiate( room1.Scene, Transform.Position, Transform.Rotation ) );
+		SpawnedRooms.Add( SceneUtility.Instantiate( room1.Scene, Transform.Position, Transform.Rotation ).GetComponent<RoomChunkComponent>( false ) );
 
 		for ( int i = 0; i < RoomCount; i++ )
 		{
@@ -30,29 +30,122 @@ public sealed class MapGeneratorComponent : BaseComponent
 
 			Vector3 PlacePoint = new Vector3( SpawnPoint.x, SpawnPoint.y, 0 );
 
-			SpawnedRooms.Add( SceneUtility.Instantiate( room.Scene, Transform.Position + PlacePoint, Transform.Rotation ) );
+			SpawnedRooms.Add( SceneUtility.Instantiate( room.Scene, Transform.Position + PlacePoint, Transform.Rotation ).GetComponent<RoomChunkComponent>( false ) );
 		}
-
-		//MoveRooms();
 
 		base.OnStart();
 	}
 
-	public async void MoveRooms()
+	bool CorrectedRooms;
+
+	public async void GenerateRoomConnections()
 	{
+		Random random = new Random(); // Create a random number generator
+
 		foreach ( var room in SpawnedRooms )
 		{
-			foreach ( var room2 in SpawnedRooms )
+			var position = room.Transform.Position;
+			var gridX = MathF.Floor( position.x / SnapGridSize );
+			var gridY = MathF.Floor( position.y / SnapGridSize );
+
+			float closestDistance = float.MaxValue;
+
+			RoomChunkComponent closestNeighbor = null;
+
+			await GameTask.Delay( 10 );
+
+			// Check for neighboring grid points and create connections
+			foreach ( var neighbor in SpawnedRooms )
 			{
-				if ( room.GetBounds().Overlaps( room2.GetBounds() ) )
+				if ( neighbor == room || neighbor.ConnectedRooms.Contains( room ) ) continue; // Skip self and already-connected rooms
+
+				var neighborPosition = neighbor.Transform.Position;
+				var neighborGridX = MathF.Floor( neighborPosition.x / SnapGridSize );
+				var neighborGridY = MathF.Floor( neighborPosition.y / SnapGridSize );
+
+				if ( (gridX == neighborGridX && Math.Abs( gridY - neighborGridY ) == 1) ||
+						(gridY == neighborGridY && Math.Abs( gridX - neighborGridX ) == 1) )
 				{
-					room.Transform.Position -= room.Transform.Position - room2.Transform.Position;
+					await GameTask.Delay( 10 );
+					// Create a connection between the two rooms
+					room.GetComponent<RoomChunkComponent>().AddConnection( neighbor.GetComponent<RoomChunkComponent>() );
+				}
+
+				// Calculate the distance between the current room and the neighbor
+				float distance = Vector3.DistanceBetween( position, neighborPosition );
+
+				if ( distance < closestDistance )
+				{
+					closestNeighbor = neighbor.GetComponent<RoomChunkComponent>();
+					closestDistance = distance;
 				}
 			}
+
+			// Randomly create a connection with the closest neighbor
+			if ( closestNeighbor is not null )
+			{
+				await GameTask.Delay( 10 );
+				room.GetComponent<RoomChunkComponent>().AddConnection( closestNeighbor );
+			}
+		}
+
+		List<RoomChunkComponent> unconnectedRooms = new List<RoomChunkComponent>( SpawnedRooms );
+
+		while ( unconnectedRooms.Count > 0 )
+		{
+			// Start with a random unconnected room
+			var startingRoom = unconnectedRooms[random.Next( unconnectedRooms.Count )];
+
+			List<RoomChunkComponent> connectedRooms = new List<RoomChunkComponent>();
+			Stack<RoomChunkComponent> roomStack = new Stack<RoomChunkComponent>();
+
+			roomStack.Push( startingRoom );
+			connectedRooms.Add( startingRoom );
+			unconnectedRooms.Remove( startingRoom );
+
+			while ( roomStack.Count > 0 )
+			{
+				var currentRoom = roomStack.Pop();
+
+				// Find unconnected neighboring rooms
+				var unconnectedNeighbors = GetUnconnectedNeighbors( currentRoom, unconnectedRooms );
+
+				if ( unconnectedNeighbors.Count > 0 )
+				{
+					// Randomly select one unconnected neighbor
+					var randomIndex = random.Next( unconnectedNeighbors.Count );
+					var randomNeighbor = unconnectedNeighbors[randomIndex];
+
+					// Connect the current room to the selected neighbor
+					currentRoom.AddConnection( randomNeighbor );
+
+					// Mark the neighbor as connected
+					connectedRooms.Add( randomNeighbor );
+					unconnectedRooms.Remove( randomNeighbor );
+
+					// Add the neighbor to the stack for further exploration
+					roomStack.Push( randomNeighbor );
+				}
+			}
+
+			await GameTask.Delay( 10 );
 		}
 	}
 
-	bool CorrectedRooms;
+	private List<RoomChunkComponent> GetUnconnectedNeighbors( RoomChunkComponent room, List<RoomChunkComponent> unconnectedRooms )
+	{
+		List<RoomChunkComponent> unconnectedNeighbors = new List<RoomChunkComponent>();
+
+		foreach ( var neighbor in room.GetComponent<RoomChunkComponent>().ConnectedRooms )
+		{
+			if ( unconnectedRooms.Contains( neighbor ) )
+			{
+				unconnectedNeighbors.Add( neighbor );
+			}
+		}
+
+		return unconnectedNeighbors;
+	}
 
 	public override void Update()
 	{
@@ -63,14 +156,15 @@ public sealed class MapGeneratorComponent : BaseComponent
 			{
 				foreach ( var room2 in SpawnedRooms )
 				{
-					if ( room.GetBounds().Overlaps( room2.GetBounds() ) )
+					if ( room == room2 ) continue;
+					if ( room.GameObject.GetBounds().Overlaps( room2.GameObject.GetBounds() ) )
 					{
 						overlaps++;
-						room.Transform.Position += (room.Transform.Position - room2.Transform.Position) * 0.3f;
+						room.Transform.Position += (room.Transform.Position - room2.Transform.Position) * 0.325f;
 					}
 				}
 			}
-			if ( overlaps == 11 )
+			if ( overlaps == 0 )
 			{
 				CorrectedRooms = true;
 				Log.Info( "Rooms corrected!" );
@@ -82,6 +176,8 @@ public sealed class MapGeneratorComponent : BaseComponent
 					room.Transform.Position = GridSnappedPosition;
 					room.GetComponent<RoomChunkComponent>( false ).SetupCollision();
 				}
+
+				GenerateRoomConnections();
 			}
 		}
 	}
--- code/Game/Map/RoomChunkComponent.cs
+++ code/Game/Map/RoomChunkComponent.cs
@@ -1,13 +1,52 @@
 using Sandbox;
+using System.Collections.Generic;
 
 public sealed class RoomChunkComponent : BaseComponent
 {
 	[Property] GameObject CameraTrigger { get; set; }
+	public List<RoomChunkComponent> ConnectedRooms { get; private set; } = new List<RoomChunkComponent>(); // List to store connected rooms
+
+	Color ThisColor;
+
+	public override void OnAwake()
+	{
+		base.OnAwake();
+		ThisColor = new Color( Game.Random.Float(), Game.Random.Float(), Game.Random.Float() ).WithAlpha( 1 );
+	}
+
+	public override void DrawGizmos()
+	{
+		foreach ( var item in ConnectedRooms )
+		{
+			Gizmo.Draw.Color = ThisColor;
+
+			Gizmo.Draw.Line( Vector3.Zero, item.Transform.Position - Transform.Position );
+		}
+	}
 
 	public void SetupCollision()
 	{
 		CameraTrigger.GetComponent<CameraTriggerComponent>( false ).RecalcBounds();
 		GetComponent<ModelCollider>( false ).Enabled = true;
 		Log.Info( "Collision fixed!" );
+	}
+
+	public bool connected;
+
+	// Method to add a connection to another room
+	public void AddConnection( RoomChunkComponent otherRoom )
+	{
+		if ( !connected )
+		{
+			if ( !ConnectedRooms.Contains( otherRoom ) && !otherRoom.ConnectedRooms.Contains( this ) )
+			{
+				ConnectedRooms.Add( otherRoom );
+			}
+		}
+	}
+
+	public override void Update()
+	{
+		base.Update();
 	}
 }
