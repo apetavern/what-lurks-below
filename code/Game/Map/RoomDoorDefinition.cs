dfad0b9d914281c08d8d6fd685d7efcc7eedb5c5
Brian Lindenhof <brian@shadowbrain.net>
2023-10-15 19:06:46 +0200

    Add little door gizmos

--- code/Game/Map/RoomDoorDefinition.cs
+++ code/Game/Map/RoomDoorDefinition.cs
@@ -2,5 +2,23 @@ using Sandbox;
 
 public sealed class RoomDoorDefinition : BaseComponent
 {
+	public override void DrawGizmos()
+	{
+		base.DrawGizmos();
+		var triangle = new Triangle();
+		triangle.A = Vector3.Backward * 15f;
+		triangle.B = Vector3.Left * 25f;
+		triangle.C = Vector3.Forward * 15f;
+		Gizmo.Draw.Color = Color.Red;
+		Gizmo.Draw.LineTriangle( triangle );
 
+		Gizmo.Draw.LineThickness = 1f;
+		Gizmo.Draw.Line( Vector3.Backward * 6f, Vector3.Right * 25f + Vector3.Backward * 7.5f );
+
+		Gizmo.Draw.LineThickness = 1f;
+		Gizmo.Draw.Line( Vector3.Right * 25f + Vector3.Backward * 7.5f, Vector3.Right * 25f + Vector3.Forward * 7.5f );
+
+		Gizmo.Draw.LineThickness = 1f;
+		Gizmo.Draw.Line( Vector3.Forward * 6f, Vector3.Right * 25f + Vector3.Forward * 7.5f );
+	}
 }
