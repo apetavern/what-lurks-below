84e96c578da233b29a34d1ff4bf2e987746f35a2
Brian Lindenhof <brian@shadowbrain.net>
2023-10-17 12:35:54 +0200

    Enemy script formatting + basic hit react

--- code/Game/Enemy/EnemyController.cs
+++ code/Game/Enemy/EnemyController.cs
@@ -6,70 +6,81 @@ namespace BrickJam.Game;
 
 public class EnemyController : BaseComponent
 {
-    [Property] public float Speed { get; set; } = 1f;
-    [Property] public float Friction { get; set; } = 0.5f;
-    [Property] public float AggroRange { get; set; } = 1f;
+	[Property] public float Speed { get; set; } = 1f;
+	[Property] public float Friction { get; set; } = 0.5f;
+	[Property] public float AggroRange { get; set; } = 1f;
 
-    [Property] public GameObject Body { get; set; }
+	[Property] public GameObject Body { get; set; }
 
-    public bool IsAggro = false;
+	public bool IsAggro = false;
 
-    GameObject Player;
-    CharacterController _characterController;
+	GameObject Player;
+	CharacterController _characterController;
 
-    public override void OnEnabled()
-    {
-        base.OnStart();
+	public override void OnEnabled()
+	{
+		base.OnStart();
 
-        Player = Scene.GetAllObjects( true ).FirstOrDefault( x => x.Name == "player" );
+		Player = Scene.GetAllObjects( true ).FirstOrDefault( x => x.Name == "player" );
 
-        // Destroy on death
-        HealthComponent healthComponent = GameObject.GetComponent<HealthComponent>();
-        healthComponent.OnDeath += () => GameObject.Destroy();
-    }
+		// Destroy on death
+		HealthComponent healthComponent = GameObject.GetComponent<HealthComponent>();
+		healthComponent.OnDeath += () => GameObject.Destroy();
+		healthComponent.OnDamage += OnDamaged;
 
-    public override void Update()
-    {
-        base.Update();
 
-        Vector3 myPosition = Transform.Position;
-        Vector3 playerPosition = Player.Transform.Position;
-        IsAggro = playerPosition.Distance( myPosition ) <= AggroRange;
+	}
 
-        _characterController ??= GameObject.GetComponent<CharacterController>();
+	public TimeSince TimeSinceDamage;
 
-        if ( IsAggro )
-        {
-            // Move towards player when in range
-            Vector3 direction = (playerPosition - myPosition).Normal;
-            Vector3 wishVelocity = (direction * Speed).WithZ( 0 );
-            _characterController.Accelerate( wishVelocity );
-            _characterController.ApplyFriction( Friction );
-        }
-        else
-        {
-            // Slow down when out of range
-            _characterController.ApplyFriction( Friction / 3f );
-        }
+	public void OnDamaged()
+	{
+		_characterController.Velocity = -Transform.Rotation.Forward * 100f;
+		TimeSinceDamage = 0f;
+	}
 
-        _characterController.Move();
+	public override void Update()
+	{
+		base.Update();
 
-        // Rotate body towards velocity
-        if ( _characterController.Velocity.LengthSquared > 0.1f )
-        {
-            Rotation targetRotation = Rotation.LookAt( _characterController.Velocity.WithZ( 0 ), Vector3.Up );
-            Body.Transform.Rotation = Rotation.Lerp( Body.Transform.Rotation, targetRotation, Time.Delta * 10f );
-        }
-    }
+		Vector3 myPosition = Transform.Position;
+		Vector3 playerPosition = Player.Transform.Position;
+		IsAggro = (playerPosition.Distance( myPosition ) <= AggroRange) || TimeSinceDamage < 10f;
 
-    public override void DrawGizmos()
-    {
-        base.DrawGizmos();
+		_characterController ??= GameObject.GetComponent<CharacterController>();
 
-        if ( !Gizmo.IsSelected ) return;
+		if ( IsAggro )
+		{
+			// Move towards player when in range
+			Vector3 direction = (playerPosition - myPosition).Normal;
+			Vector3 wishVelocity = (direction * Speed).WithZ( 0 );
+			_characterController.Accelerate( wishVelocity );
+			_characterController.ApplyFriction( Friction );
+		}
+		else
+		{
+			// Slow down when out of range
+			_characterController.ApplyFriction( Friction / 3f );
+		}
 
-        // Draw red sphere for aggro range
-        Gizmo.Draw.Color = Color.Red;
-        Gizmo.Draw.LineSphere( new() { Center = Vector3.Zero, Radius = AggroRange }, 8 );
-    }
+		_characterController.Move();
+
+		// Rotate body towards velocity
+		if ( _characterController.Velocity.LengthSquared > 0.1f )
+		{
+			Rotation targetRotation = Rotation.LookAt( playerPosition - myPosition, Vector3.Up );
+			Body.Transform.Rotation = Rotation.Lerp( Body.Transform.Rotation, targetRotation, Time.Delta * 10f );
+		}
+	}
+
+	public override void DrawGizmos()
+	{
+		base.DrawGizmos();
+
+		if ( !Gizmo.IsSelected ) return;
+
+		// Draw red sphere for aggro range
+		Gizmo.Draw.Color = Color.Red;
+		Gizmo.Draw.LineSphere( new() { Center = Vector3.Zero, Radius = AggroRange }, 8 );
+	}
 }
