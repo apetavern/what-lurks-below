2c205aff44c3843ecddc8819a9cf52d6dd7960a0
trundlr <57276947+trundlr@users.noreply.github.com>
2023-10-21 15:31:37 -0300

    SubtleCameraMovementComponent stub

---
+++ code/Game/SubtleCameraMovementComponent.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox;
+
+namespace BrickJam.Game;
+
+public class SubtleCameraMovementComponent : BaseComponent
+{
+	public override void Update()
+	{
+		base.Update();
+		
+		// Use mouse delta to give a minor parallax effect on the camera rotation
+		var camera = Scene.GetComponent<CameraComponent>( true, true );
+		
+	}
+}
