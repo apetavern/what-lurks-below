a6742b67a40e5c9fe1c40cedf1ca20f0437f1a86
trundlr <57276947+trundlr@users.noreply.github.com>
2023-10-21 17:34:08 -0300

    City ambience

---
+++ code/Game/MenuSoundComponent.cs
@@ -0,0 +1,22 @@
+﻿using Sandbox;
+
+namespace BrickJam.Game;
+
+public class MenuSoundComponent : BaseComponent
+{
+	private Sound _sound;
+	
+	public override void OnStart()
+	{
+		base.OnStart();
+
+		_sound = Sound.FromScreen( "city_ambience" );
+	}
+
+	public override void OnDestroy()
+	{
+		base.OnDestroy();
+
+		_sound.SetVolume( 0 );
+	}
+}
