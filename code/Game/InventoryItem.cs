aa3277bb04d20f330713ec771733550fe603a84a
trundlr <57276947+trundlr@users.noreply.github.com>
2023-10-21 16:23:53 -0300

    stub out actions

--- code/Game/InventoryItem.cs
+++ code/Game/InventoryItem.cs
@@ -23,6 +23,27 @@ public class InventoryItem : GameResource
 	public string ImagePath { get; set; }
 	
 	public List<InventoryAction> InventoryActions { get; set; }
+
+	public void DoAction( InventoryAction action )
+	{
+		if ( action is InventoryAction.Examine )
+		{
+			Log.Info( ExamineText );
+		}
+		else if ( action is InventoryAction.Drop )
+		{
+			// make all items droppable without requiring a unique pickup
+		}
+		else if ( action is InventoryAction.Equip )
+		{
+			// manually check item type and hardcode correlation to weapon class
+			// then equip
+		}
+		else if ( action is InventoryAction.Use )
+		{
+			// manually check item type and hardcode behaviour
+		}
+	}
 }
 
 public enum InventoryAction
