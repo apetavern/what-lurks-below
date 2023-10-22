067a72e8e88e1c1dfd90ed37e91577905f640939
trundlr <57276947+trundlr@users.noreply.github.com>
2023-10-22 20:19:24 -0300

    Add background music loop

--- code/Game/SoundManagerComponent.cs
+++ code/Game/SoundManagerComponent.cs
@@ -6,12 +6,16 @@ namespace BrickJam.Game;
 public class SoundManagerComponent : BaseComponent
 {
 	private TimeUntil TimeUntilWaterDrip { get; set; } = 1.0f;
+	private Sound Music { get; set; }
 
 	public override void OnStart()
 	{
 		base.OnStart();
 
 		TimeUntilWaterDrip = 1.0f;
+
+		Music = Sound.FromScreen( "spoop" );
+		Music.SetVolume( 0f );
 	}
 
 	public override void Update()
@@ -21,5 +25,14 @@ public class SoundManagerComponent : BaseComponent
 			Sound.FromScreen( "water_drip" );
 			TimeUntilWaterDrip = Random.Shared.Float( 1.6f, 2.4f );
 		}
+
+		Music.SetVolume( 1f );
+	}
+
+	public override void OnDestroy()
+	{
+		base.OnDestroy();
+
+		Music.Stop();
 	}
 }
