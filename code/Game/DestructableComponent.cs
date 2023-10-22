8a082710b9d0c65681407a5d2a73b3ebca55adb0
trundlr <57276947+trundlr@users.noreply.github.com>
2023-10-22 15:42:38 -0300

    some stuff

--- code/Game/DestructableComponent.cs
+++ code/Game/DestructableComponent.cs
@@ -1,19 +1,28 @@
+using System;
+using System.Collections.Generic;
+using BrickJam;
 using BrickJam.Player;
 using Sandbox;
 
 public sealed class DestructableComponent : BaseComponent
 {
+	private List<InventoryItem> PotentialDrops { get; set; } = new();
+	
 	public override void OnStart()
 	{
 		// Destroy on death
 		HealthComponent healthComponent = GameObject.GetComponent<HealthComponent>();
 		healthComponent.OnDeath += OnDeath;
 		healthComponent.OnDamage += OnDamaged;
+
+		var invItems = ResourceLibrary.GetAll<InventoryItem>();
+		PotentialDrops.AddRange( invItems );
 	}
 
 	public void OnDeath()
 	{
 		Sound.FromWorld( "barrel_break", Transform.Position );
+		Log.Info( PotentialDrops[Random.Shared.Next(0, PotentialDrops.Count - 1)]  );
 		GameObject.Destroy();
 	}
 
