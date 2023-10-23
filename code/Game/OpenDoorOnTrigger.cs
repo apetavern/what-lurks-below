923f48f176dd2b86ee99a5eaf1c4e4491252114d
Brian Lindenhof <brian@shadowbrain.net>
2023-10-23 10:31:28 +0200

    Add basic key => door functionality

---
+++ code/Game/OpenDoorOnTrigger.cs
@@ -0,0 +1,27 @@
+using BrickJam.Game;
+using Sandbox;
+using System.Linq;
+
+public sealed class OpenDoorOnTrigger : BaseComponent
+{
+	bool OpenedDoor;
+
+	public override void OnStart()
+	{
+		GetComponent<CameraTriggerComponent>().OnTriggered += CheckForKey;
+	}
+
+	public void CheckForKey()
+	{
+		if ( !OpenedDoor )
+		{
+			var player = Scene.GetAllObjects( true ).Where( X => X.Name == "player" ).FirstOrDefault();
+			if ( player.GetComponent<PlayerFlagsComponent>().HasBossKey )
+			{
+				OpenedDoor = true;
+				GameObject.Parent.GetComponent<BezierAnimationComponent>( false, true ).AnimateObject( GameObject.Parent.GetComponent<ModelComponent>( false, true ).GameObject, 2f, true );
+				Destroy();
+			}
+		}
+	}
+}
